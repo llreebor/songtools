cb66ad13781abfafdce5ed7f23afecce00cc1659
mac <mac@Roman-Timoshchuk.local>
2022-11-16 19:25:03 +0200

    base

---
+++ js/main.js
@@ -0,0 +1,119 @@
+// Reviews Slider
+if (document.querySelector('.swiper__reviews')) {
+    const swiper = new Swiper('.swiper__reviews', {
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1.3,
+                spaceBetween: 20,
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+        },
+    })
+}
+
+// Buttons
+if (document.querySelector('.club__buttons')) {
+    const btns = document.querySelectorAll('.club__button')
+    const button = document.querySelector('.club__buttons')
+    btns[0].classList.add('active')
+    button.addEventListener('click', () => {
+        button.classList.toggle('active')
+
+        if (!button.classList.contains('active')) {
+            btns[0].classList.add('active')
+            btns[1].classList.remove('active')
+        } else {
+            btns[0].classList.remove('active')
+            btns[1].classList.add('active')
+        }
+    })
+    // btns.forEach((btn, i) => {
+    //     btns[i].classList.remove('active')
+    //     btns[0].classList.add('active')
+    //     document.querySelector('.club__buttons').classList.remove('active')
+
+    //     btn.addEventListener('click', (e) => {
+    //         btns.forEach((item) => item.classList.remove('active'))
+    //         e.target.classList.add('active')
+    //         button.classList.toggle('active')
+    //     })
+    // })
+}
+
+const cs = getComputedStyle(document.documentElement, null)
+const slider = document.getElementById('card-slider')
+const cards = Array.from(slider.querySelectorAll('.slider-item'))
+const lastSlide = cards[cards.length - 1]
+cards.unshift(slider.insertBefore(lastSlide.cloneNode(true), slider.firstChild))
+let defaultX = cs.getPropertyValue('--slider-width'),
+    defaultY = cs.getPropertyValue('--slider-height')
+const getAnimations = () => [
+    [0.5, { x: -defaultX, y: -defaultY, opacity: 0, fontSize: 0 }],
+    [0.5, { x: 0, y: 0, opacity: 0.18, fontSize: '28px' }],
+    [0.5, { x: defaultX, y: defaultY, opacity: 0.3, fontSize: '32px' }],
+    [
+        0.5,
+        {
+            x: defaultX * 2,
+            y: defaultY * 2,
+            opacity: 1,
+            fontSize: '2.25em',
+        },
+    ],
+    [0.5, { x: defaultX, y: defaultY * 3, opacity: 0.7, fontSize: '2em' }],
+    [
+        0.5,
+        {
+            x: 0,
+            y: defaultY * 4,
+            opacity: 0.3,
+            fontSize: '1.75em',
+            onComplete: updateArray,
+        },
+    ],
+]
+
+let animations = getAnimations()
+
+const images = Array.from(
+    document.querySelectorAll('#image-slider .slider-item'),
+)
+let currentIndex = 0
+
+function startAnim() {
+    cards.forEach((slide, index) => {
+        TweenMax.to(slide, ...animations[index])
+    })
+    images.forEach((image, index) => {
+        TweenMax.to(image, 0.5, {
+            autoAlpha: index === currentIndex ? 1 : 0,
+        })
+    })
+    currentIndex = currentIndex < images.length - 1 ? currentIndex++ : 0
+
+    images.push(images.shift())
+}
+
+startAnim()
+
+function updateArray() {
+    defaultX = cs.getPropertyValue('--slider-width')
+    defaultY = cs.getPropertyValue('--slider-height')
+    animations = getAnimations()
+    const firstElem = cards.shift()
+    TweenMax.to(firstElem, 0, { x: -defaultX, y: defaultY * 5, opacity: 0 })
+    firstElem.innerHTML = cards[0].innerHTML
+    cards.push(firstElem)
+    setTimeout(function () {
+        startAnim()
+    }, 3000)
+}
